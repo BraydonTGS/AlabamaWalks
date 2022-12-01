f1fffc9a2a331f8330e92f30649119e579835a65
Braydon Sutherland <braydonsutherland@gmail.com>
2022-12-01 11:00:30 -0600

    Injected DbContext into Services Collection, Created Initial Migration, Updated Database

--- AlabamaWalks.API/Program.cs
+++ AlabamaWalks.API/Program.cs
@@ -2,12 +2,21 @@
 
 // Creates the WebApplication builder class //
 // We can use to inject the dependencies into the services collection //
+using AlabamaWalks.API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container //
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Injecting the DbContext Class into the Services Collection //
+builder.Services.AddDbContext<AlabamaWalksDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("AlabamaWalks"));
+});
 
 var app = builder.Build();
 
