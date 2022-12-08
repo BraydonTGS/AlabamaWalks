dbbf14bf3ddec35cf5e9db8c6508c88e5c39d5e5
Braydon Sutherland <braydonsutherland@gmail.com>
2022-12-08 17:14:40 -0600

    created LoginAsync method on my AuthController, Injected my IUserRepository with the UserRepository implementation. Created a LoginRequest dto. Added fluent validation for the Login Request. Tested the validation and successfull, Tested bad request and successfull. Setting up for JWT Tolken now

--- AlabamaWalks.API/Controllers/AuthController.cs
+++ AlabamaWalks.API/Controllers/AuthController.cs
@@ -1,12 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AlabamaWalks.API.Interfaces;
+using AlabamaWalks.API.Models.DTO;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AlabamaWalks.API.Controllers
 {
+    [ApiController]
+    [Route("[controller]")]
     public class AuthController : Controller
     {
-        public IActionResult Index()
+        private readonly IUserRepository _repository;
+
+        public AuthController(IUserRepository repository)
         {
-            return View();
+            _repository = repository;
+        }
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> LoginAsync(LoginRequest request)
+        {   // Validation Set using Fluent Validations // 
+
+            // Check if user is authenticated //
+            var response =  await _repository.AuthenticateUserAsync(request.UserName, request.Password);
+            if (response)
+            {
+                // Generate Jwt Token //
+            }
+
+            return BadRequest("Invalid Username or Password"); 
         }
     }
 }
---
+++ AlabamaWalks.API/Validators/LoginRequestValidator.cs
@@ -0,0 +1,14 @@
+﻿using AlabamaWalks.API.Models.DTO;
+using FluentValidation;
+
+namespace AlabamaWalks.API.Validators
+{
+    public class LoginRequestValidator : AbstractValidator<LoginRequest>
+    {
+        public LoginRequestValidator()
+        {
+            RuleFor(x => x.UserName).NotEmpty();
+            RuleFor(x => x.Password).NotEmpty();
+        }
+    }
+}
