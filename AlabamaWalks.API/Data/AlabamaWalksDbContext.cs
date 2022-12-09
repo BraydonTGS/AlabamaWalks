6a542ed5edd7430175ef3f1bbf66a4d1a89be537
Braydon Sutherland <braydonsutherland@gmail.com>
2022-12-09 09:05:54 -0600

    created a role and user_role model. added db sets to alabamawalkdbcontext. override OnModelCreating to establish my many to many relationships

--- AlabamaWalks.API/Data/AlabamaWalksDbContext.cs
+++ AlabamaWalks.API/Data/AlabamaWalksDbContext.cs
@@ -11,6 +11,24 @@ namespace AlabamaWalks.API.Data
         public DbSet<Region> Regions { get; set; }
         public DbSet<Walk> Walks { get; set; }
         public DbSet<WalkDifficulty> WalkDifficulty { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+
+        public DbSet<User_Role> User_Roles { get; set; }
+
+
+        // Overriding the OnModelCreating to define our Many to Many Relationships //
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User_Role>()
+                .HasOne(x => x.Role)
+                .WithMany(y => y.UserRoles)
+                .HasForeignKey(x => x.RoleId);
+            modelBuilder.Entity<User_Role>()
+                .HasOne(x => x.User)
+                .WithMany(y => y.UserRoles)
+                .HasForeignKey(x => x.UserId); 
+        }
 
 
     }
--- AlabamaWalks.API/Models/Domain/User.cs
+++ AlabamaWalks.API/Models/Domain/User.cs
@@ -6,9 +6,13 @@
         public string UserName { get; set; }
         public string EmailAddress { get; set; }
         public string Password { get; set; }
-        public List<string> Roles { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
 
+        // Many to Many Relationship //
+        // Navigation Property //
+        public List<User_Role> UserRoles { get; set; }
+
+        
     }
 }
